41fbad1fc537852626f6ebe43bccb9fc79910217
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2024-03-13 14:44:04 +0100

    Add language support

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -121,6 +121,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGES = (
+    ("en-us", "English"),
+    ("ru-ru", "Russian"),
+)
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
